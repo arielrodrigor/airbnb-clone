83b90f0669c66585fe55a0806110177fea6e3a56
Ariel Ramirez <ariel.ramirez.reyes@gmail.com>
2023-02-20 21:03:03 -0600

    Banner component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Header from "@/components/Header";
+import Banner from "@/components/Banner";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function Home() {
           {/*Header*/}
           <Header />
           {/*Banner*/}
+          <Banner />
 
 
       </div>
